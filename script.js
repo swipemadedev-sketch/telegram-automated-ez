14b49350a4cabca7815e68cf70ee835c1209f4dd
swipemadedev-sketch <swipemadedev@gmail.com>
2025-10-03 21:26:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
-// Initialize the Telegram Web App
+// Initialize the Telegram Web App object
 const tg = window.Telegram.WebApp;
 
-// Get the form element
+// Get form elements
 const form = document.getElementById('ticket-form');
 const subjectInput = document.getElementById('subject');
 const descriptionInput = document.getElementById('description');
 
-// Configure the Main Button
+// --- Main Button Configuration ---
+
+// Set the text of the main button
 tg.MainButton.text = "Submit Ticket";
+// Make the main button visible
 tg.MainButton.show();
 
-// Event listener for the Main Button
+// This function is called when the user clicks the main button
 tg.MainButton.onClick(() => {
-    // Check if form is valid
+    // Check if the form is valid (all required fields are filled)
     if (!form.checkValidity()) {
         tg.showAlert('Please fill out all fields.');
         return;
     }
 
-    // Create the data object to send
+    // Create the data object to send to the bot
     const data = {
         subject: subjectInput.value,
-        description: descriptionInput.value
-        // Telegram will automatically send user data along with this
+        description: descriptionInput.value,
     };
 
-    // Use WebApp.sendData() to send the data to the bot
-    // This data will be available in the 'web_app_data' field of the update
+    // Use WebApp.sendData() to send data to the bot.
+    // This will trigger a 'web_app_data' event on the backend.
     tg.sendData(JSON.stringify(data));
-
-    // After sending data, you can close the Mini App
-    tg.close();
+    
+    // The Mini App will be closed automatically after sendData is called.
 });
